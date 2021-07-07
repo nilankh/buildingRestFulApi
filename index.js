c23dcc6209ac25271989ccb59437c648616471ec
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-07 23:04:15 +0530

    debugging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const config = require('config');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const startupDebugger = require('debug')('app:startup')
+const dbDebugger = require('debug')('app:db')
+const config = require('config')
+const morgan = require('morgan')
+const helmet = require('helmet')
 const Joi = require('joi')
 //  this return a function we called as express
-const logger = require('./logger');
+const logger = require('./logger')
 
 const express = require('express')
 // now we need to call express function and store in app by convention
@@ -16,26 +18,28 @@ const app = express()
 app.use(express.json())
 // when we call express.json() method, this method returns a function, a middleware function, the job of this middleware function is to read the request, and if there is json object in the body of the request, it will parse the body of the request, into json object and then it will set to the req.body property.
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 // when we call express.urlencoded() method,this method return  a function a middleware function, this middleware function parses incoming request with urlencoded payloads, that is req with body like this key=value&key=value
 
 app.use(express.static('public'))
 // it is use to serve static files, passed a folder called public.We will put all our static assets like css, images and so on inside this folder.
-app.use(helmet());
+app.use(helmet())
 
 // Configuration
 console.log('Application Name: ' + config.get('name'))
 console.log('Mail Server: ' + config.get('mail.host'))
 console.log('Mail Password: ' + config.get('mail.password'))
 
-
 // app.use(morgan('tiny'))
 // enable loggng http request only on development macchine
-if(app.get('env') === 'development'){
-
+if (app.get('env') === 'development') {
   app.use(morgan('tiny'))
-  console.log('Morgan enabled')
+  // console.log('Morgan enabled')
+  startupDebugger('Morgan enabled')
 }
+
+// Db work...
+dbDebugger('Connected to the database..')
 // to set on production set NODE_ENV = production write on terminal
 
 // Custom middleware function
@@ -44,7 +48,7 @@ if(app.get('env') === 'development'){
 //   next();
 //   // next() to pass control to the next middleware function in the pipeline.If you don't do this, you are not termination req, res cycle, our req will end up hanging.
 // });
-app.use(logger);
+app.use(logger)
 app.use(function (req, res, next) {
   console.log('Authenticating...')
   next()
